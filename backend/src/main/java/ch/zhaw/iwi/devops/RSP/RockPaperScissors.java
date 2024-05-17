47c5c944a07554a7bc819a6932d4c15a0743feb1
Kurmajoe <kurmajoe@students.zhaw.ch>
2024-05-17 10:31:24 +0200

    all passes

--- backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
@@ -2,14 +2,18 @@ package ch.zhaw.iwi.devops.RSP;
 
 public class RockPaperScissors {
     public String play(String player1, String player2) {
+        if (!isValidInput(player1) || !isValidInput(player2)) return "Invalid input!";
+        
+        if (player1.equals(player2)) return "Draw!";
+        
         if (player1.equals("rock") && player2.equals("scissors")) return "Player 1 wins!";
         if (player1.equals("scissors") && player2.equals("paper")) return "Player 1 wins!";
         if (player1.equals("paper") && player2.equals("rock")) return "Player 1 wins!";
         
-        if (player2.equals("rock") && player1.equals("scissors")) return "Player 2 wins!";
-        if (player2.equals("scissors") && player1.equals("paper")) return "Player 2 wins!";
-        if (player2.equals("paper") && player1.equals("rock")) return "Player 2 wins!";
-        
-        return "Invalid!";
+        return "Player 2 wins!";
+    }
+
+    private boolean isValidInput(String input) {
+        return input.equals("rock") || input.equals("paper") || input.equals("scissors");
     }
 }
