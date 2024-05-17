43c6139e96b3a479902928c13e27567f4137c73f
Kurmajoe <kurmajoe@students.zhaw.ch>
2024-05-17 10:30:59 +0200

    even more passes

--- backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
@@ -6,6 +6,10 @@ public class RockPaperScissors {
         if (player1.equals("scissors") && player2.equals("paper")) return "Player 1 wins!";
         if (player1.equals("paper") && player2.equals("rock")) return "Player 1 wins!";
         
+        if (player2.equals("rock") && player1.equals("scissors")) return "Player 2 wins!";
+        if (player2.equals("scissors") && player1.equals("paper")) return "Player 2 wins!";
+        if (player2.equals("paper") && player1.equals("rock")) return "Player 2 wins!";
+        
         return "Invalid!";
     }
 }
