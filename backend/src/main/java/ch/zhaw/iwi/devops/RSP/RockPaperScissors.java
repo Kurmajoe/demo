f51bb865a590dd654c487dac13af5b19d6ba71b6
Kurmajoe <kurmajoe@students.zhaw.ch>
2024-05-17 10:29:37 +0200

    updated to get some successes

--- backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/RSP/RockPaperScissors.java
@@ -2,7 +2,10 @@ package ch.zhaw.iwi.devops.RSP;
 
 public class RockPaperScissors {
     public String play(String player1, String player2) {
-        // Return a dummy value to ensure all tests fail
+        if (player1.equals("rock") && player2.equals("scissors")) return "Player 1 wins!";
+        if (player1.equals("scissors") && player2.equals("paper")) return "Player 1 wins!";
+        if (player1.equals("paper") && player2.equals("rock")) return "Player 1 wins!";
+        
         return "Invalid!";
     }
 }
